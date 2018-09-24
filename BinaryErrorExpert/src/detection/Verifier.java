c66ed8f1f34e77a59e2c9f8eafe77e8461f641a4
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-09-24 20:20:45 -0300

    Add new methods to the interface

--- BinaryErrorExpert/src/detection/Verifier.java
+++ BinaryErrorExpert/src/detection/Verifier.java
@@ -1,7 +1,19 @@
 package detection;
 
+import util.Response;
+
 public interface Verifier {
 
+	public boolean verifyParity(int[] data);
+	
 	public String verifyData(int[] data);
+	
+	default String getParityMessage(int[] data) {
+		if (this.verifyParity(data)) {
+			return Response.CORRECT.toString();
+		}
+		
+		return Response.INCORRECT.toString();
+	}
 	
 }
