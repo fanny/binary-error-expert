2f553da855c3f762d87894568dcc07fd2cc880d3
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-09-25 06:29:12 -0300

    Add tests

---
+++ BinaryErrorExpert/src/tests/correction/HammingTest.java
@@ -0,0 +1,30 @@
+package tests.correction;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import correction.Hamming;
+
+class HammingTest {
+	
+	private Hamming hamming;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.hamming = new Hamming(8);
+	}
+
+	@Test
+	void testFixTenBit() {
+		int[] data = { 0,1,1,1,0,0,1,0,1,1,1,0};
+		int[] expectedValue = {0,1,1,1,0,0,1,0,1,0,1,0};
+		
+		assertArrayEquals(expectedValue, hamming.fixData(data));
+		
+	}
+
+}
---
+++ BinaryErrorExpert/src/tests/detection/ParityVerifier.java
@@ -0,0 +1,19 @@
+package tests.detection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ParityVerifier {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
